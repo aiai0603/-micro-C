f05df2497171e7b6e67408b073622bd8fe1d729b
张帅 <1016868503@qq.com>
2021-06-20 13:34:27 +0800

    将bug说明添加到测试用例中了

--- microc/test/test-init.c
+++ microc/test/test-init.c
@@ -1,3 +1,7 @@
+/*
+bug:全局变量不能附初值
+Int  i =1;
+*/
 Void main(Int n) {
   
     Int a = 7;
@@ -5,7 +9,7 @@ Void main(Int n) {
     String s = "12121212";
     Float f = 12.32;
     
-
+    //print("%d",i);
     print("%d",a);
     print("%c",b);
     println(s);
